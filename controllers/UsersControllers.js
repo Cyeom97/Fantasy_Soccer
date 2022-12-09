1698fa2a90060f3a4bf7246eec0cd6d1b581042c
Christopher Yeom <chrisyeom97@gmail.com>
2022-12-08 23:04:23 -0500

    created put route for users

--- controllers/UsersControllers.js
+++ controllers/UsersControllers.js
@@ -9,6 +9,19 @@ const GetUsers = async (req, res) => {
   }
 }
 
+const UpdateUser = async (req, res) => {
+  try {
+    let userId = parseInt(req.params.id)
+    let user = await User.update(req.body, {
+      where: { id: userId }
+    })
+    res.send(user)
+  } catch (error) {
+    throw error
+  }
+}
+
 module.exports = {
-  GetUsers
+  GetUsers,
+  UpdateUser
 }
--- routes/UsersRouter.js
+++ routes/UsersRouter.js
@@ -2,5 +2,6 @@ const router = require('express').Router()
 const controller = require('../controllers/UsersControllers')
 
 router.get('/', controller.GetUsers)
+router.put('/:id', controller.UpdateUser)
 
 module.exports = router

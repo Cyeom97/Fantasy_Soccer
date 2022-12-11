2834585d1edf803fd5d8342ff0406c497d0384b8
Christopher Yeom <chrisyeom97@gmail.com>
2022-12-10 22:21:19 -0500

    created boilerplate for allplayers.js

---
+++ client/src/pages/AllPlayers.js
@@ -0,0 +1,17 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import { BASE_URL } from '../services/api'
+
+const AllPlayers = () => {
+  const [players, setPlayers] = useState([])
+
+  useEffect(() => {}, [])
+
+  return (
+    <div>
+      <div></div>
+    </div>
+  )
+}
+
+export default AllPlayers

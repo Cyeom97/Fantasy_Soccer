6530d83c0343eb203f3a6eb6d416c99648808ecf
Christopher Yeom <chrisyeom97@gmail.com>
2022-12-11 18:49:52 -0500

    created back button for nav

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,7 +1,9 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+
+
 
 const Nav = ({ authenticated, user, handleLogout }) => {
-    
+    let navigate = useNavigate()
     let authenticatedOptions
   if (user) {
     authenticatedOptions = (
@@ -11,6 +13,7 @@ const Nav = ({ authenticated, user, handleLogout }) => {
         <Link to="/teams">Teams</Link>
         <Link to="/players">Players</Link>
         <Link to="/pickteam">PickTeam</Link>
+        <p className="back" onClick={() => navigate(-1)}>Back</p>
         <Link onClick={handleLogout} to="/">Sign Out</Link>
       </nav>
     )
@@ -22,6 +25,7 @@ const Nav = ({ authenticated, user, handleLogout }) => {
       <Link to="/">Home</Link>
       <Link to="teams">Teams</Link>
       <Link to="/players">Players</Link>
+      <p className="back" onClick={() => navigate(-1)}>Back</p>
       <Link to="/Register">Register</Link>
       <Link to="/Signin">Sign In</Link>
     </nav>

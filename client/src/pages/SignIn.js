37dda92c5bea4c71fec79c3fdf892795411cac8b
Christopher Yeom <chrisyeom97@gmail.com>
2022-12-13 14:30:05 -0500

    styling sign in page

--- client/src/pages/SignIn.js
+++ client/src/pages/SignIn.js
@@ -36,82 +36,86 @@ const SignIn = (props) => {
   }
 
   return (
-    <div className="signinMessage">
-      <h1>Hello</h1>
-      <p>
-        Sign in to your FantasySoccer account or{' '}
-        <Link to="/Register">create an account</Link>
-      </p>
-      <div className="sign">
-        <form className="col" onSubmit={handleSubmit}>
-          <div className="input-wrapper">
-            <input
-              className="email"
-              onChange={handleChange}
-              name="email"
-              type="email"
-              placeholder="example@example.com"
-              value={formValues.email}
-              required
-            />
-          </div>
-          <div className="input-wrapper">
-            <input
-              className="password"
-              onChange={handleChange}
-              type="password"
-              name="password"
-              placeholder="Password"
-              value={formValues.password}
-              required
-            />
-          </div>
-          <div className="button1">
-            <button
-              className="regButton"
-              disabled={!formValues.email || !formValues.password}
-            >
-              Sign In
-            </button>
-          </div>
-        </form>
+    <body>
+      <div className="main">
+        <input type="checkbox" id="chk" aria-hidden="true" />
+        <div className="signup">
+          <label>
+            Hello Sign in to your FantasySoccer account or{' '}
+            <Link to="/Register">create an account</Link>
+          </label>
+          <form className="col" onSubmit={handleSubmit}>
+            <div className="input-wrapper">
+              <input
+                className="email"
+                onChange={handleChange}
+                name="email"
+                type="email"
+                placeholder="example@example.com"
+                value={formValues.email}
+                required
+              />
+            </div>
+            <div className="input-wrapper">
+              <input
+                className="password"
+                onChange={handleChange}
+                type="password"
+                name="password"
+                placeholder="Password"
+                value={formValues.password}
+                required
+              />
+            </div>
+            <div className="button1">
+              <button
+                className="regButton"
+                disabled={!formValues.email || !formValues.password}
+              >
+                Sign In
+              </button>
+            </div>
+          </form>
+        </div>
+        <div className="Admin">
+          <form onSubmit={adminSubmit}>
+            <label for="chk" aria-hidden="true">
+              Admin Login
+            </label>
+            <div className="input-wrapper">
+              <input
+                className="email"
+                onChange={adminChange}
+                name="email"
+                type="email"
+                placeholder="example@example.com"
+                value={adminForm.email}
+                required
+              />
+            </div>
+            <div className="input-wrapper">
+              <input
+                className="password"
+                onChange={adminChange}
+                type="password"
+                name="password"
+                placeholder="Password"
+                value={adminForm.password}
+                required
+              />
+            </div>
+            <div className="button1">
+              <button
+                className="regButton"
+                disabled={!adminForm.email || !adminForm.password}
+              >
+                Sign In
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
-      <h2>Admin</h2>
-      <div className="sign">
-        <form className="col" onSubmit={adminSubmit}>
-          <div className="input-wrapper">
-            <input
-              className="email"
-              onChange={adminChange}
-              name="email"
-              type="email"
-              placeholder="example@example.com"
-              value={adminForm.email}
-              required
-            />
-          </div>
-          <div className="input-wrapper">
-            <input
-              className="password"
-              onChange={adminChange}
-              type="password"
-              name="password"
-              placeholder="Password"
-              value={adminForm.password}
-              required
-            />
-          </div>
-          <div className="button1">
-            <button
-              className="regButton"
-              disabled={!adminForm.email || !adminForm.password}
-            >
-              Sign In
-            </button>
-          </div>
-        </form>
-      </div>
-    </div>
+    </body>
   )
 }
 

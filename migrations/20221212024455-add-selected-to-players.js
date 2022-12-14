1941d21af8eb5db0e5e5fac869e8d9a2c6fb0f68
Christopher Yeom <chrisyeom97@gmail.com>
2022-12-14 12:02:16 -0500

    commented out the up in migration

--- migrations/20221212024455-add-selected-to-players.js
+++ migrations/20221212024455-add-selected-to-players.js
@@ -1,14 +1,14 @@
-// 'use strict'
+'use strict'
 
-// /** @type {import('sequelize-cli').Migration} */
-// module.exports = {
-//   async up(queryInterface, Sequelize) {
-//     return queryInterface.addColumn('players', 'price', {
-//       type: Sequelize.STRING
-//     })
-//   },
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    // return queryInterface.addColumn('players', 'price', {
+    //   type: Sequelize.STRING
+    // })
+  },
 
-//   async down(queryInterface, Sequelize) {
-//     return queryInterface.removeColumn('players', 'selected')
-//   }
-// }
+  async down(queryInterface, Sequelize) {
+    return queryInterface.removeColumn('players', 'selected')
+  }
+}

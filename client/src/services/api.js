a20d47447ba8c3a0ab7dca0a77362364df6181a0
Christopher Yeom <chrisyeom97@gmail.com>
2022-12-14 10:54:34 -0500

    moved package.json to root

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import Axios from 'axios'
 
-export const BASE_URL = 'http://localhost:3001/'
+export const BASE_URL = 'https://soccer-manager1.herokuapp.com/'
 
 const Client = Axios.create({ baseURL: BASE_URL })
 
